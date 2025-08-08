a4d2d630df6aa7799760af611835065f1d9fee7f
combari <combarim2@gmail.com>
2025-08-08 13:15:08 +0100

    Initial commit

---
+++ src/main/java/enset/ma/palmrecognitionserverapp/PalmRecognitionServerAppApplication.java
@@ -0,0 +1,13 @@
+package enset.ma.palmrecognitionserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmRecognitionServerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalmRecognitionServerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/enset/ma/palmrecognitionserverapp/PalmRecognitionServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.ma.palmrecognitionserverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmRecognitionServerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
